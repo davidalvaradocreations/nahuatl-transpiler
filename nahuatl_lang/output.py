b957c28def2c6808a96c0011602b8120f51a2d70
David Alvarado <davida.create@proton.me>
2025-07-14 00:43:18 -0700

    Add files via upload

---
+++ nahuatl_lang/output.py
@@ -0,0 +1,8 @@
+def calcula():
+    x = 5 + 3 * 2
+    y = (10 - 4) / 2
+    print("x = ", x)
+    print("y = ", y)
+    return True
+
+calcula()
---
+++ nahuatl_lang/output_program.py
@@ -0,0 +1,8 @@
+def tlachia():
+    print("Niltze, macehualmeh!")
+    return True
+
+if True:
+    tlachia()
+else:
+    print("Ahmor True.")
